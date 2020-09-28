b79ab890c5ef5bb899488c2656da667f4107aad3
lukesrw <lukesrw+github@gmail.com>
2020-09-28 22:36:37 +0100

    Stripped array, implement first version, debugging

--- function_new.php
+++ function_new.php
@@ -2,221 +2,72 @@
 $sample_data = file_get_contents('./data/user-agents.json');
 $sample_data = json_decode($sample_data, true);
 
-$found_words = array();
-
-$regex_matches = array(
-    '/2345explorer/' => '2345 Explorer',
-    '/360[s|e]e/' => '360 Browser',
-    '/nintendo 3ds/' => 'Nintendo 3DS Browser',
-    '/kuaiso/' => '7 Star Browser',
-    '/abolimba/' => 'Abolimba',
-    '/abrowse\b/' => 'ABrowse',
-    '/acoo browser/' => 'Acoo Browser',
-    '/alienforce/' => 'Alienforce',
-    '/amaya/' => 'Amaya',
-    '/amiga-aweb/' => 'Amiga Aweb',
-    '/amigavoyager/' => 'Amiga Voyager',
-    '/amigo/' => 'Amigo',
-    '/antfresco/' => 'ANT Fresco',
-    '/antgalio/' => 'ANT Galio',
-    '/aol\b/' => 'AOL Explorer',
-    '/aolshield/' => 'AOL Shield',
-    '/arora/' => 'Arora',
-    '/avant(\s|b)/' => 'Avant Browser',
-    '/avast|asw/' => 'Avast Secure Browser',
-    '/aviator/' => 'Aviator',
-    '/avirascout/' => 'Avira Scout',
-    '/baidubrowser/' => 'Baidu Browser',
-    '/spark\//' => 'Baidu Spark',
-    '/basilisk/' => 'Basilisk',
-    '/beamrise/' => 'Beamrise',
-    '/beonex/' => 'Beonex',
-    '/blackbird/' => 'Blackbird',
-    '/blackhawk/' => 'BlackHawk',
-    '/bolt/' => 'Bolt',
-    '/brave/' => 'Brave',
-    '/briskbard/' => 'BriskBard',
-    '/browzar/' => 'Browzar',
-    '/bunjalloo/' => 'Bunjalloo',
-    '/camino|chimera/' => 'Camino',
-    '/charon/' => 'Charon',
-    '/chedot/' => 'Chedot',
-    '/cheshire/' => 'Cheshire',
-    '/headlesschrome/' => 'Chrome Headless',
-    '/chromium/' => 'Chromium',
-    '/\sdragon\b/' => 'Chromodo',
-    '/classilla/' => 'Classila',
-    '/lbbrowser|acheetahi|liebaofast/' => 'CM Browser',
-    '/corom|coc_coc_browser/' => 'Coc Coc',
-    '/columbus/' => 'Columbus',
-    '/cometbird/' => 'CometBird',
-    '/comodo_dragon/' => 'Comodo Dragon',
-    '/conkeror/' => 'Conkeror',
-    '/coolnovo/' => 'CoolNovo',
-    '/crazy browser/' => 'Crazy Browser',
-    '/cuam/' => 'Cuam',
-    '/cunaguaro/' => 'Cunaguaro',
-    '/cyberdog/' => 'Cyberdog',
-    '/cyberfox/' => 'Cyberfox',
-    '/deepnet explorer/' => 'Deepnet Explorer',
-    '/demeter/' => 'Demeter',
-    '/deskbrowse/' => 'DeskBrowse',
-    '/dillo/' => 'Dillo',
-    '/doczilla/' => 'DocZilla',
-    '/dooble/' => 'Dooble',
-    '/dplus/' => 'DPlus',
-    '/edbrowse/' => 'Edbrowse',
-    '/elinks/' => 'Elinks',
-    '/enigma browser/' => 'Enigma Browser',
-    '/epic/' => 'Epic',
-    '/escape/' => 'Espial TV Browser',
-    '/firebird/' => 'Firebird',
-    '/bonecho|granparadiso|lorentz|minefield|namoroka|shiretoko/' => 'Firefox',
-    '/fireweb navigator/' => 'Fireweb Navigator',
-    '/flock/' => 'Flock',
-    '/fluid/' => 'Fluid',
-    '/framafox/' => 'Framafox',
-    '/navigateur web/' => 'Freebox Browser',
-    '/freeu/' => 'FreeU',
-    '/galeon/' => 'Galeon',
-    '/globalmojo/' => 'GlobalMojo',
-    '/epiphany/' => 'GNOME Web',
-    '/greenbrowser/' => 'GreenBrowser',
-    '/hotjava/' => 'HotJava',
-    '/hv3/' => 'Hv3',
-    '/hydra browser/' => 'Hydra Browser',
-    '/ibm webexplorer/' => 'IBM WebExplorer',
-    '/ibrowse\b/' => 'IBrowse',
-    '/icab/' => 'iCab',
-    '/ice\s?browser/' => 'ICE Browser',
-    '/iceape/' => 'IceApe',
-    '/icecat/' => 'IceCat',
-    '/icedragon/' => 'IceDragon',
-    '/iceweasel/' => 'IceWeasel',
-    '/internetsurfboard/' => 'InternetSurfboard',
-    '/irider/' => 'iRider',
-    '/iridium/' => 'Iridium',
-    '/iron/' => 'Iron',
-    '/k-meleon/' => 'K-Meleon',
-    '/k-ninja/' => 'K-Ninja',
-    '/kapiko/' => 'Kapiko',
-    '/kazehakase/' => 'Kazehakase',
-    '/kinza/' => 'Kinza',
-    '/strata/' => 'Kirix Strata',
-    '/kkman/' => 'KKman',
-    '/konqueror/' => 'Konqueror',
-    '/kylo/' => 'Kylo',
-    '/lbrowser/' => 'LBrowser',
-    '/cern-linemode/' => 'Line Mode Browser',
-    '/links\s/' => 'Links',
-    '/lobo/' => 'Lobo',
-    '/lolifox/' => 'lolifox',
-    '/lovense/' => 'Lovense Browser',
-    '/lunascape/' => 'Lunascape',
-    '/lynx/' => 'Lynx',
-    '/madfox/' => 'Madfox',
-    '/maple/' => 'Maple Browser',
-    '/maxthon|mxbrowser|myie2/' => 'Maxthon',
-    '/edge/' => 'Microsoft Edge',
-    '/midori/' => 'Midori',
-    '/min\//' => 'Min',
-    '/minibrowser/' => 'Mini Browser',
-    '/multi-browser/' => 'Multi-Browser XP',
-    '/multizilla/' => 'MultiZilla',
-    '/mxnitro/' => 'MxNitro',
-    '/myibrow/' => 'My Internet Browser',
-    '/ncsa(_|\s)mosaic/' => 'NCSA Mosaic',
-    '/netbox|netgem/' => 'NetBox',
-    '/netcaptor/' => 'NetCaptor',
-    '/netpositive/' => 'NetPositive',
-    '/netscape|navigator/' => 'Netscape Navigator',
-    '/netsurf/' => 'NetSurf',
-    '/offbyone/' => 'Off By One',
-    '/omniweb/' => 'OmniWeb',
-    '/opera|opr|oupeng|\smms\/\d/' => 'Opera',
-    '/juzibrowser/' => 'Orange Browser',
-    '/orca/' => 'Orca',
-    '/otter/' => 'Otter',
-    '/owb/' => 'Odyssey Web Browser',
-    '/palemoon/' => 'Pale Moon',
-    '/patriott/' => 'Patriott',
-    '/perk/' => 'Perk',
-    '/phaseout/' => 'Phaseout',
-    '/phoenix/' => 'Phoenix',
-    '/^pb/' => 'PirateBrowser',
-    '/polarity/' => 'Polarity',
-    '/playstation 4/' => 'PS4 Web Browser',
-    '/puffin/' => 'Puffin',
-    '/qiyu/' => 'Qiyu',
-    '/qqbrowser/' => 'QQbrowser',
-    '/qtweb/' => 'QtWeb',
-    '/qupzilla/' => 'QupZilla',
-    '/qutebrowser/' => 'qutebrowser',
-    '/nichrome/' => 'Rambler Browser',
-    '/rekonq/' => 'Rekonq',
-    '/roccat/' => 'Roccat Browser',
-    '/rockmelt/' => 'RockMelt',
-    '/ryouko/' => 'Ryouko',
-    '/saayaa/' => 'SaaYaa Explorer',
-    '/salam browser/' => 'Salam Browser',
-    '/smart-tv/' => 'Samsung TV Browser',
-    '/seamonkey/' => 'SeaMonkey',
-    '/secure/' => 'Secure Browser'
-);
-
-$skip_browsers = array(
-    'Amigo',
-    'Chrome',
-    'Firefox',
-    'Internet Explorer',
-    'Mozilla',
-    'Safari'
-);
-
-$regex = false;
-// $regex = '/\smms\/\d/';
-
-$last_browser = false;
-
-echo '<pre>';
-
-foreach ($sample_data as $browser => $data) {
-    if (in_array($browser, $skip_browsers)) {
-        continue;
+function parseUserAgent($user_agent = null, $debug = false)
+{
+    static $regexes = false;
+    if (! $regexes) {
+        $regexes = json_decode(
+            file_get_contents('./data/regex.json'),
+            true
+        );
     }
 
-    foreach ($data['list'] as $example) {
-        $example = strtolower($example);
+    $user_agent = strtolower($user_agent);
+    $result = array(
+        'info' => array(
+            'weight' => 0
+        )
+    );
 
-        foreach (array_keys($regex_matches) as $regex_match) {
-            if (preg_match($regex_match, $example)) {
-                continue 2;
+    foreach ($regexes as $regex => $info) {
+        if (
+            preg_match($regex, $user_agent, $matches) &&
+            $info['weight'] > $result['info']['weight']
+        ) {
+            if ($debug) {
+                echo '<pre>' . print_r($info, true) . '</pre>';
             }
+
+            $result['info'] = $info;
+            $result['matches'] = $matches;
         }
+    }
 
-        if (!$regex || preg_match($regex, $example, $matches)) {
-            if (isset($matches)) {
-                foreach ($matches as $match) {
-                    $example = str_replace(
-                        $match,
-                        '<b>' . $match . '</b>',
-                        $example
-                    );
-                }
+    return $result;
+}
+
+$user_agent = '';
+
+if ($user_agent) {
+    parseUserAgent($user_agent, true);
+} else {
+    foreach ($sample_data as $browser => $info) {
+        $browser = str_replace(
+            array(
+                ' mobile',
+                'mobile '
+            ),
+            array(
+                '',
+                ''
+            ),
+            strtolower($browser)
+        );
+
+        foreach ($info['list'] as $user_agent) {
+            $result = parseUserAgent($user_agent);
+
+            if (
+                $browser != strtolower($result['info']['browser_name']) &&
+                substr($browser, 0, strlen($result['info']['browser_name'])) != strtolower($result['info']['browser_name'])
+            ) {
+                echo sprintf(
+                    'Incorrect for "%1$s" with "%2$s" - said %3$s',
+                    $browser,
+                    $user_agent,
+                    $result['info']['browser_name']
+                ) . '<br/>';
             }
-
-            if ($last_browser != $browser) {
-                if ($last_browser) {
-                    echo '<hr/>';
-                }
-                $last_browser = $browser;
-            }
-
-            echo sprintf(
-                'Matching "%1$s" (%2$s)' . "\n",
-                $browser,
-                $example
-            );
         }
     }
 }
