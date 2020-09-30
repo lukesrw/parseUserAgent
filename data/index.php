a1bf791e88e53713ef42fc4f743bb49caca1fb08
lukesrw <lukesrw+github@gmail.com>
2020-10-01 00:04:43 +0100

    Updated paths

--- data/index.php
+++ data/index.php
@@ -3,10 +3,10 @@
  * Strip JSON pretty print from regex_raw
  */
 file_put_contents(
-    './data/regex.json',
+    './regex.json',
     json_encode(
         json_decode(
-            file_get_contents('./data/regex_raw.json'),
+            file_get_contents('./regex_raw.json'),
             true
         )
     )
