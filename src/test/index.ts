929481910faacfa048916de7869489c007a2fb2c
lukesrw <lukesrw+github@gmail.com>
2020-10-02 21:57:08 +0100

    Refactor into mocha/chai

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,26 +1,45 @@
-import { parseUserAgent } from "../index";
+/*global context describe it*/
 
+/**
+ * Test data
+ */
+const user_agents: TestUAListInterface = require("../../../data/user-agents.json");
+
+/**
+ * Interfaces (for testing)
+ */
 import { TestUAListInterface } from "../interfaces/user-interface-test";
 
-const user_agents: TestUAListInterface = require("../../../data/user-agents.json");
+/**
+ * Npm packages (for testing)
+ */
+import * as chai from "chai";
+
+/**
+ * Self
+ */
+import { parseUserAgent } from "..";
 
 for (let browser_name in user_agents) {
     if (Object.prototype.hasOwnProperty.call(user_agents, browser_name)) {
-        user_agents[browser_name].list.forEach(user_agent => {
-            let result = parseUserAgent(user_agent);
+        context(`Parsing "${browser_name}" User Agents`, () => {
+            user_agents[browser_name].list.forEach(user_agent => {
+                let result = parseUserAgent(user_agent);
 
-            browser_name = browser_name
-                .replace(/\bmobile\b/giu, "")
-                .replace(/\s{2}/gu, " ")
-                .trim();
+                browser_name = browser_name
+                    .replace(/\bmobile\b/giu, "")
+                    .replace(/\s{2}/gu, " ")
+                    .trim()
+                    .toLowerCase();
 
-            if (
-                result.browser_name.toLowerCase() !== browser_name.toLowerCase()
-            ) {
-                console.log(
-                    `Said "${result.browser_name}" instead of "${browser_name}" for "${user_agent}"`
-                );
-            }
+                describe(`Parsing "${user_agent}"`, () => {
+                    it("Parsing Successful", () => {
+                        chai.expect(result.browser_name.toLowerCase()).to.equal(
+                            browser_name
+                        );
+                    });
+                });
+            });
         });
     }
 }
