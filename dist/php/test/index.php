565724758f8c88b41aa2fc93e2c39bf4add0311d
lukesrw <lukesrw+github@gmail.com>
2020-10-02 21:56:10 +0100

    Moved strtolower

--- dist/php/test/index.php
+++ dist/php/test/index.php
@@ -10,19 +10,21 @@ foreach ($user_agents as $browser_name => $info) {
     foreach ($info['list'] as $user_agent) {
         $result = parseUserAgent($user_agent);
 
-        $browser_name = trim(
-            preg_replace(
-                '/\s{2}/u',
-                " ",
+        $browser_name = strtolower(
+            trim(
                 preg_replace(
-                    '/\bmobile\b/ui',
-                    '',
-                    $browser_name
+                    '/\s{2}/u',
+                    " ",
+                    preg_replace(
+                        '/\bmobile\b/ui',
+                        '',
+                        $browser_name
+                    )
                 )
             )
         );
 
-        if (strtolower($result['browser_name']) !== strtolower($browser_name)) {
+        if (strtolower($result['browser_name']) !== $browser_name) {
             echo sprintf(
                 'Said "%1$s" instead of "%2$s" for "%3$s"' . "\n",
                 $result['browser_name'],
