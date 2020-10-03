239a6c5a9b4ae9d8466f1ff653dfa7046259cfca
lukesrw <lukesrw+github@gmail.com>
2020-10-03 13:01:41 +0100

    Updated interfaces for flexibility

--- src/interfaces/user-interface.ts
+++ src/interfaces/user-interface.ts
@@ -4,13 +4,31 @@ export interface BrowserInterface {
     w: number;
 }
 
-export interface BrowserParserInterface {
-    [regex: string]: BrowserInterface;
+export interface OperatingSystemInterface {
+    osn: string;
+    osv?: string;
+    w: number;
 }
 
-export interface ParsedUAInterface {
+export interface BrowserParserInterface {
+    bn: {
+        [regex: string]: BrowserInterface;
+    };
+    osn: {
+        [regex: string]: OperatingSystemInterface;
+    };
+}
+
+export interface ParsedBrowserInterface {
     browser_name: string;
     browser_version: string;
-    // operating_system_name: string;
-    // operating_system_version: string;
 }
+
+export interface ParsedOperatingSystemInterface {
+    operating_system_name: string;
+    operating_system_version: string;
+}
+
+export interface ParsedUAInterface
+    extends ParsedBrowserInterface,
+        ParsedOperatingSystemInterface {}
