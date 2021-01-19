3d6b82e7d2cf28d380c9efb963d01d9169ca0ddc
lukesrw <lukesrw+github@gmail.com>
2021-01-19 19:56:07 +0000

    Simplified to remove output limit

--- src/test/index.ts
+++ src/test/index.ts
@@ -24,6 +24,7 @@ import { parseUserAgent } from "..";
 let result: {
     [user_agent: string]: ParsedUAInterface;
 } = {};
+let errors = 0;
 
 ["Browser", "Operating System"].forEach(category => {
     let key: keyof TestUAListInterface;
@@ -32,63 +33,65 @@ let result: {
     } else {
         key = "operating_system";
     }
+    for (let name in user_agents[key]) {
+        if (Object.prototype.hasOwnProperty.call(user_agents[key], name)) {
+            for (let version in user_agents[key][name]) {
+                if (
+                    Object.prototype.hasOwnProperty.call(
+                        user_agents[key][name],
+                        version
+                    )
+                ) {
+                    // eslint-disable-next-line no-loop-func
+                    user_agents[key][name][version].forEach(user_agent => {
+                        if (
+                            !Object.prototype.hasOwnProperty.call(
+                                result,
+                                user_agent
+                            )
+                        ) {
+                            result[user_agent] = parseUserAgent(user_agent);
+                        }
 
-    describe(`Parsing ${category}s`, () => {
-        for (let name in user_agents[key]) {
-            if (Object.prototype.hasOwnProperty.call(user_agents[key], name)) {
-                for (let version in user_agents[key][name]) {
-                    if (
-                        Object.prototype.hasOwnProperty.call(
-                            user_agents[key][name],
-                            version
-                        )
-                    ) {
-                        context(
-                            `Parsing "${name}" (${version}) User Agents`,
-                            // eslint-disable-next-line no-loop-func
-                            () => {
-                                user_agents[key][name][version].forEach(
-                                    user_agent => {
-                                        if (
-                                            !Object.prototype.hasOwnProperty.call(
-                                                result,
-                                                user_agent
-                                            )
-                                        ) {
-                                            result[user_agent] = parseUserAgent(
-                                                user_agent
-                                            );
-                                        }
+                        if (
+                            String(
+                                result[user_agent][
+                                    `${key}_name` as keyof ParsedUAInterface
+                                ]
+                            ) !== String(name) ||
+                            String(
+                                result[user_agent][
+                                    `${key}_version` as keyof ParsedUAInterface
+                                ]
+                            ) !== String(version)
+                        ) {
+                            console.table({
+                                category: key,
+                                user_agent,
+                                [result[user_agent][
+                                    `${key}_name` as keyof ParsedUAInterface
+                                ] as string]: name,
+                                [result[user_agent][
+                                    `${key}_version` as keyof ParsedUAInterface
+                                ] as string]: version
+                            });
 
-                                        if (
-                                            String(
-                                                result[user_agent][
-                                                    `${key}_name` as keyof ParsedUAInterface
-                                                ]
-                                            ) !== String(name) ||
-                                            String(
-                                                result[user_agent][
-                                                    `${key}_version` as keyof ParsedUAInterface
-                                                ]
-                                            ) !== String(version)
-                                        ) {
-                                            console.table({
-                                                user_agent,
-                                                [result[user_agent][
-                                                    `${key}_name` as keyof ParsedUAInterface
-                                                ] as string]: name,
-                                                [result[user_agent][
-                                                    `${key}_version` as keyof ParsedUAInterface
-                                                ] as string]: version
-                                            });
-                                        }
-                                    }
-                                );
-                            }
-                        );
-                    }
+                            errors += 1;
+                        }
+                    });
+                    // context(
+                    //     `Parsing "${name}" (${version}) User Agents`,
+                    //     // eslint-disable-next-line no-loop-func
+                    //     () => {
+                    //     }
+                    // );
                 }
             }
         }
-    });
+    }
+
+    console.log(errors);
+
+    // describe(`Parsing ${category}s`, () => {
+    // });
 });
